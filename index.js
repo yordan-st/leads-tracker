ad8eb56f044ae3145da6f29bab0d3455413e70c9
Yordan Stoyanov <y.s.stoyanov@hotmail.com>
2025-04-17 16:20:55 +0200

    refactor: preparing database connection

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let myLeads = []
-let storedLeads = JSON.parse(localStorage.getItem('myLeads'))
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-75ea0-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+// let myLeads = []
+// let storedLeads = JSON.parse(localStorage.getItem('myLeads'))
 
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
@@ -29,18 +39,19 @@ function addLead(local) {
     let newLink = `<a href="${inputEl.value}" target="_blank">${inputEl.value}</a>`
     leadsList.innerHTML += "<li>" + newLink + "</li>"
     inputEl.value = ''
-    localStorage.setItem('myLeads', JSON.stringify(local))
+    // save it to db
+    // localStorage.setItem('myLeads', JSON.stringify(local))
 }
 
-function saveTab(local) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        local.push(tabs[0].url)
-        let newLink = `<a href="${tabs[0].url}" target="_blank">${tabs[0].url}</a>`
-        leadsList.innerHTML += "<li>" + newLink + "</li>"
-        inputEl.value = ''
-        localStorage.setItem('myLeads', JSON.stringify(local))
-    })
-}
+// function saveTab(local) {
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         local.push(tabs[0].url)
+//         let newLink = `<a href="${tabs[0].url}" target="_blank">${tabs[0].url}</a>`
+//         leadsList.innerHTML += "<li>" + newLink + "</li>"
+//         inputEl.value = ''
+//         localStorage.setItem('myLeads', JSON.stringify(local))
+//     })
+// }
 
 function clearLeads(local) {
     if(local.length > 0   ) {
@@ -53,5 +64,5 @@ function clearLeads(local) {
 }
 
 inputBtn.addEventListener('click', () => addLead(myLeads));
-tabBtn.addEventListener('click', () => saveTab(myLeads));
+// tabBtn.addEventListener('click', () => saveTab(myLeads));
 deleteBtn.addEventListener('click', () => clearLeads(myLeads));
